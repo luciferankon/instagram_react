641f786247250dce8fb6d1979f16de2b04869db6
Ankon <44019225+luciferankon@users.noreply.github.com>
2019-04-14 16:51:01 +0530

    added uniq key for every post

--- src/posts.js
+++ src/posts.js
@@ -4,7 +4,11 @@ class Posts extends React.Component {
   render() {
     return (
       <div className='posts'>
-        {this.props.posts.map(post => <div className="post">{post}</div>)}
+        {this.props.posts.map((post, id) => (
+          <div className='post' key={id + "post"}>
+            {post}
+          </div>
+        ))}
       </div>
     );
   }
